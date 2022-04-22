d33afca9435eba9c9957a24a65005309efea00ec
Daniel Ayzenshteyn <ayzendan@post.bgu.ac.il>
2022-04-22 17:23:56 +0300

    first commit

---
+++ IntegrityStatement.java
@@ -0,0 +1,9 @@
+
+
+public class IntegrityStatement {
+
+	public static String signature() {
+		return "Daniel Ayzenshteyn and Ron Shefland";
+	}
+	
+}
---
+++ Warmup.java
@@ -0,0 +1,18 @@
+
+
+public class Warmup {
+    public static int backtrackingSearch(int[] arr, int x, int forward, int back, Stack myStack) {
+        // TODO: implement your code here
+    	return -987; // temporal return command to prevent compilation error 
+    }
+
+    public static int consistentBinSearch(int[] arr, int x, Stack myStack) {
+        // TODO: implement your code here
+    	
+    	// Your implementation should contain a this line:
+    	int inconsistencies = Consistency.isConsistent(arr);
+    	
+    	return -987; // temporal return command to prevent compilation error
+    }
+    
+}
