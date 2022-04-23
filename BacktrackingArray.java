0256491ff93f55f47a0bc95612e2473106213526
Daniel Ayzenshteyn <ayzendan@post.bgu.ac.il>
2022-04-23 16:56:33 +0300

    ver1

--- BacktrackingArray.java
+++ BacktrackingArray.java
@@ -3,40 +3,74 @@
 public class BacktrackingArray implements Array<Integer>, Backtrack {
     private Stack stack;
     private int[] arr;
-    // TODO: implement your code here
+    private int count;
 
     // Do not change the constructor's signature
     public BacktrackingArray(Stack stack, int size) {
         this.stack = stack;
         arr = new int[size];
+        count=0;
     }
 
     @Override
     public Integer get(int index){
-        // TODO: implement your code here
-    	return null; // temporal return command to prevent compilation error
+        if(0<=index && index<count)
+            return arr[index];
+        return -1;
     }
 
     @Override
     public Integer search(int k) {
-        // TODO: implement your code here
-    	return null; // temporal return command to prevent compilation error
+        return binarySearch(0,count,k);
+    }
+
+    private Integer binarySearch(int low,int high,int k){
+        if(high>=low){
+            int middle=low+(high-low)/2;
+            if(arr[middle]==k)
+                return middle;
+            else if(arr[middle]>k)
+                return binarySearch(low,middle-1,k);
+            else return binarySearch(middle+1,high,k);
+        }
+        else return -1;
     }
 
     @Override
     public void insert(Integer x) {
-        // TODO: implement your code here
+        if(count==arr.length)
+            throw new RuntimeException("the array is full");
+        arr[count]=x;
+        count++;
     }
 
     @Override
     public void delete(Integer index) {
-        // TODO: implement your code here
+        if(0<=index && index<count){
+            int j=index;
+            while(j<count-1) {
+                arr[j]=arr[j+1];
+                j++;
+            }
+            count--;
+        }
     }
 
     @Override
     public Integer minimum() {
-        // TODO: implement your code here
-    	return null; // temporal return command to prevent compilation error
+        if (count==0)
+            return -1;
+        int minIndex=0;
+        int min=arr[0];
+        int i=1;
+        while(i<count){
+            if(arr[i]<min){
+                min=arr[i];
+                minIndex=i;
+            }
+            i++;
+        }
+        return minIndex;
     }
 
     @Override
--- Warmup.java
+++ Warmup.java
@@ -2,16 +2,30 @@
 
 public class Warmup {
     public static int backtrackingSearch(int[] arr, int x, int forward, int back, Stack myStack) {
-        // TODO: implement your code here
-    	return -987; // temporal return command to prevent compilation error 
+        int index=0;
+        while(index<arr.length){
+            for(int i=0;i<forward && index<arr.length;i++){
+                if(arr[index]==x)
+                    return index;
+                myStack.push(arr[index]);
+                index++;
+            }
+            for(int j=0;j<back;j++){
+                if(arr[index]==x)
+                    return index;
+                myStack.pop();
+                index--;
+            }
+        }
+        return -1;
     }
 
     public static int consistentBinSearch(int[] arr, int x, Stack myStack) {
         // TODO: implement your code here
-    	
+
     	// Your implementation should contain a this line:
     	int inconsistencies = Consistency.isConsistent(arr);
-    	
+
     	return -987; // temporal return command to prevent compilation error
     }
     
